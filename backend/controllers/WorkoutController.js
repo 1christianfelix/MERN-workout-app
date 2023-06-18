31b0c61580e107865ad42cdfd821ca2bcdfa56fb
Christian Felix <1christianfelix@gmail.com>
2023-06-17 18:39:44 -0700

    Added controllers for update and delete

--- backend/controllers/WorkoutController.js
+++ backend/controllers/WorkoutController.js
@@ -1,4 +1,5 @@
 const Workout = require("../models/WorkoutModel");
+const mongoose = require("mongoose");
 
 // get all workouts
 const getWorkouts = async (req, res) => {
@@ -10,6 +11,11 @@ const getWorkouts = async (req, res) => {
 // get a single workout
 const getWorkout = async (req, res) => {
   const { id } = req.params;
+
+  // checking if the id being passed is a valid MongoDB type id
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "workout does not exist" });
+  }
 
   const workout = await Workout.findById(id);
 
@@ -33,10 +39,48 @@ const createWorkout = async (req, res) => {
   }
 };
 
+// delete a workout
+const deleteWorkout = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "workout does not exist" });
+  }
+
+  const workout = await Workout.findOneAndDelete({ _id: id });
+
+  if (!workout) {
+    return res.status(404).json({ error: "No such workout" });
+  }
+
+  return res.status(200).json(workout);
+};
+
 // update a workout
+const updateWorkout = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "workout does not exist" });
+  }
+
+  const workout = await Workout.findOneAndUpdate(
+    { _id: id },
+    {
+      ...req.body,
+    }
+  );
+  if (!workout) {
+    return res.status(404).json({ error: "No such workout" });
+  }
+
+  return res.status(200).json(workout);
+};
 
 module.exports = {
   getWorkout,
   getWorkouts,
   createWorkout,
+  deleteWorkout,
+  updateWorkout,
 };
