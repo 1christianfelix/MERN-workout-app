68b13489af0eb34ec1a893a183935b8f190c40f8
Christian Felix <1christianfelix@gmail.com>
2023-06-18 22:14:11 -0700

    added react-app and created navbar, and workout detail components

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+import WorkoutDetails from "../components/WorkoutDetails";
+
+const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+    fetchWorkouts();
+    console.log(workouts);
+  }, []);
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails
+              key={workout._id}
+              workout={workout}
+            ></WorkoutDetails>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
